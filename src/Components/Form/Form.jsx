0b86f406c56acc9a5167eee95aaae65e40a2bb59
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-08-31 15:08:25 +0200

    form sandbox component

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Currency } from '../Currency/Currency';
-import { Button } from '../Button/Button';
 import PropTypes from 'prop-types';
+import { Button } from '../Button/Button';
 
 export class Form extends Component {
 
@@ -12,49 +11,49 @@ export class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quota: 0,
-            currency: 'pln',
             firstName: 'Grzegorz',
             lastName: 'Martowski',
-            pesel: '88012589589',
-            age: 0
+            email: 'kontakt@stabsoft.pl',
+            phone: '88012589589'
         };
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handlePlnChange = this.handlePlnChange.bind(this);
-        this.handleEurChange = this.handleEurChange.bind(this);
-        this.handleChange = this.handleChange.bind(this);
     }
 
     handleSubmit(event) {
-        alert(`Submitted${this.state.name}`);
+        alert(`Submitted ${this.state.firstName}`);
         event.preventDefault();
     }
 
-    handlePlnChange(quota) {
-        this.setState({ currency: 'pln', quota });
-    }
-
-    handleEurChange(quota) {
-        this.setState({ currency: 'eur', quota });
-    }
-
-    handleChange(event) {
-        const { value, name } = event;
+    handleChange = ({ target: { name, value } }) => {
         this.setState({ [name]: value });
-    }
+    };
 
     render() {
-        const { quota, currency } = this.state;
-        const eur = currency === 'eur' ? quota : quota * 0.2339;
-        const pln = currency === 'pln' ? quota : quota * 4.2734;
+        const { firstName, lastName, phone, email } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
-                PLN: <Currency quota={pln} onCurrencyChange={this.handlePlnChange} />
-                EUR: <Currency quota={eur} onCurrencyChange={this.handleEurChange} />
-                <input type="text" value={this.state.age} onChange={this.handleChange} />
 
-                <Button color="#f4568a" width='100px' content="Zapisz" />
-                <button className="btn" type="submit">Submit</button>
+                <div className='form-group'>
+                    <label>First name: </label>
+                    <input type='text' name='firstName' value={firstName} onChange={this.handleChange} />
+                </div>
+
+                <div className='form-group'>
+                    <label>Last name: </label>
+                    <input type='text' name='lastName' value={lastName} onChange={this.handleChange} />
+                </div>
+
+                <div className='form-group'>
+                    <label>Email name: </label>
+                    <input type='text' name='email' value={email} onChange={this.handleChange} />
+                </div>
+
+                <div className='form-group'>
+                    <label>Phone name: </label>
+                    <input type='text' name='phone' value={phone} onChange={this.handleChange} />
+                </div>
+
+                <Button onClick={this.handleSubmit} content={'Submit'} />
             </form>
         );
     }
--- src/Components/Portal/PortalComponent.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-
-const portalRoot = document.getElementById('portal');
-
-export const Portal = props => {
-    const { children } = props;
-    return ReactDOM.createPortal(children, portalRoot);
-};
