2ba4ec4528bba67fd51396e48727ed5a10a1ecf5
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-08-31 14:18:21 +0200

    using context in themes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,37 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'spectre.css';
-import { Home } from './Home/Home';
 import './app.less';
-import { Sidebar } from '../Components/Sidebar/Sidebar';
-import AppContext from './AppContext';
+import { List } from '../Components/List/List';
 
 export class App extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            firstName: 'Grzegorz',
-            lastName: 'Martowski',
-            phoneNumber: 693881676,
-            randomImgAddress: 'https://api.thecatapi.com/v1/images/search?format=src&mime_types=image/gif'
-        };
-    }
-
     render() {
+
+        const listOfFruits = ['Orange', 'Strawberry', 'Rasberry', 'Coconut', 'Apple', 'Plum', 'Banana'];
+
         return (
-            <AppContext.Provider value={this.state}>
-                <Router>
-                    <div className="app">
-                        <Sidebar />
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route path="/about/:id" component={About} />
-                            <Route path="/contact" component={Contact} />
-                        </Switch>
-                    </div>
-                </Router>
-            </AppContext.Provider>
+            <List value={listOfFruits} />
         );
     }
 }
