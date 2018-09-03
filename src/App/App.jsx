a84f6fc9445f0f437ea69bb0bf2b32a2d2a14e9a
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-09-03 10:09:35 +0200

    cleaning project

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,54 +1,15 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Loadable } from 'react-loadable';
-import { Home } from './Home/Home';
-import { About } from './About/About';
-import { Contact } from './Contact/Contact';
+import React, { Component } from 'react';
 import 'spectre.css';
 import './app.less';
-import { Sidebar } from '../Sidebar/Sidebar';
-import AppContext from './AppContext';
 
 export class App extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            firstName: 'Grzegorz',
-            lastName: 'Martowski',
-            phoneNumber: 693881676
-        };
-        console.log('constructor');
-    }
-
-    componentWillMount() {
-        console.log('component will mount');
-    }
-
-    componentDidMount() {
-        console.log('component did mount');
-    }
-
-    componentWillUnmount() {
-        console.log('component will unmount');
-    }
-
     render() {
         return (
-            <Fragment>
-                <AppContext.Provider value={this.state}>
-                    <Router>
-                        <div className="app">
-                            <Sidebar />
-                            <Switch>
-                                <Route exact path="/" component={Home} />
-                                <Route path="/about/:id" component={About} />
-                                <Route path="/contact" component={Contact} />
-                            </Switch>
-                        </div>
-                    </Router>
-                </AppContext.Provider>
-            </Fragment>
+            <div className="app">
+                <h1>Home page</h1>
+                <div>Home content</div>
+            </div>
         );
     }
 }
