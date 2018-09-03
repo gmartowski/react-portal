35afd902fc62672efd4f8acb890cb238d0a5727a
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-09-03 10:40:34 +0200

    reduce function

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,10 +5,24 @@ import './app.less';
 export class App extends Component {
 
     render() {
+        let myArray = [1, 2, 3, 4];
+        let result = 0;
+
         return (
             <div className="app">
-                <h1>Home page</h1>
-                <div>Home content</div>
+                <h1>functional programming</h1>
+                <h2>1. reduce()</h2>
+                <div>
+                    {
+                        result = myArray.reduce((acc, val) => {
+                            console.log(`Accumulator: ${acc}`);
+                            <br />;
+                            console.log(`Current value: ${val}`);
+                            <br />;
+                            return acc + val;
+                        })
+                    }
+                </div>
             </div>
         );
     }
