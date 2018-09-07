22c5c1a91d3045297ec9b159798e469aecf7c2db
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-09-07 19:11:44 +0200

    Hoisting

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,6 +4,33 @@ import './app.less';
 
 export class App extends Component {
 
+    componentDidMount() {
+
+        // hoisting();
+
+        // function declaration
+        // function hoisting() {
+        //     console.log(hoist);
+        //
+        //     var what = "Variable and function declariations";
+        //
+        //     console.log('What is hoisted?' + what);
+        //
+        //     var hoist = 'to lift or raise up';
+        //
+        //     console.log('Hoist means ' + hoist);
+        // }
+
+        // function expression
+        var sum = function() {
+            console.log(a + b);
+        };
+        sum();
+
+        var a = 10;
+        var b = 5;
+    }
+
     render() {
         return (
             <div className="app">
