98e99501ff583c020a4d672782924df7fd6fa2be
Grzegorz Martowski <grzegorz.martowski@avra.pl>
2018-09-18 17:01:26 +0200

    call apply bind

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,6 +4,25 @@ import './app.less';
 
 export class App extends Component {
 
+    componentDidMount() {
+        var obj = { num: 2 };
+        var obj2 = { num: 5 };
+        var addToThis = function(a, b, c) {
+            return this.num + a + b + c;
+        };
+
+        // CALL
+        console.log(addToThis.call(obj, 1, 2, 3)); // przyjmuje argumenty po kolei
+
+        // APPLY
+        var arry = [1, 2, 3];
+        console.log(addToThis.apply(obj, arry)); // przyjmuje argumenty jako tablicę
+
+        // BIND
+        var bound = addToThis.bind(obj);
+        console.log(bound);
+    }
+
     render() {
         return (
             <div className="app">
